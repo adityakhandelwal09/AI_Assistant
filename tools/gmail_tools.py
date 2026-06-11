483c1a7325f26c9a99fef7cb2eeeca11e49b6cfa
adi958 <adityak0719@gmail.com>
2026-06-10 23:58:17 -0400

    get_email_content() retrieves full email content and recursively searches for text/plain body content in case of multiparts

--- tools/gmail_tools.py
+++ tools/gmail_tools.py
@@ -1,3 +1,5 @@
+import base64
+
 from config.auth import get_google_service
 service = get_google_service("gmail", "v1")
 
@@ -29,9 +31,22 @@ def search_emails(query, max_results=5):
         
     return email_list
 
-#def get_email_content(message_id):
- #   msg = service.users().messages().get(userId="me", id=message["id"], format="metadata").execute()
+#helper function to recursively search through the parts of an email payload to find the text/plain body content
+def get_body_from_parts(parts):
+    for part in parts:
+        if part.get("mimeType") == "text/plain":
+            return part.get("body", {}).get("data")
+        elif "multipart" in part.get("mimeType"):
+            subparts = part.get("parts", [])
+            body = get_body_from_parts(subparts)
+            if body:
+                return body
+    return None
 
-
-
-
+#given a message ID, retrieves the full email content (including body) and decodes it from base64
+def get_email_content(message_id):
+    msg = service.users().messages().get(userId="me", id=message_id, format="full").execute()
+    parts = msg.get("payload", {}).get("parts", [])
+    email_body = get_body_from_parts(parts)
+    print(base64.urlsafe_b64decode(email_body).decode('utf-8'))
+    return base64.urlsafe_b64decode(email_body).decode('utf-8')
