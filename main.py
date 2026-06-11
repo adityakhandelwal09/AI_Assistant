b4933d65af3b903624a0051589954de93be4a8be
adi958 <adityak0719@gmail.com>
2026-06-10 22:57:29 -0400

    search_emails() takes query and returns list of first 5 results including id, thread_id, subject, snippet, sender, recipient, and date.

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import datetime as datetime
 from dotenv import load_dotenv
 import os
 from tools.calendar_tools import get_events, create_event, delete_event
+from tools.gmail_tools import search_emails
 
 #get_events("2026-06-08")
 #create_event("Kravitz Appointment", "Wisdom teet consultation", datetime.datetime(2026, 6, 10, 15, 40), datetime.datetime(2026, 6, 10, 16, 20), False)
 #create_event("Aditi's Birthday", "", datetime.datetime(2026, 6, 19), datetime.datetime(2026, 6, 19), True)
 #delete_event("13qsl87u02874gll5c149087e8") 
+
+print(search_emails("subject:possible research opportunity"))
 
 '''
 load_dotenv()
--- tools/gmail_tools.py
+++ tools/gmail_tools.py
@@ -1,3 +1,37 @@
 from config.auth import get_google_service
 service = get_google_service("gmail", "v1")
 
+def search_emails(query, max_results=5):
+    gmail_results = service.users().messages().list(userId="me", q=query, maxResults=max_results).execute()
+    messages = gmail_results.get("messages", [])
+    email_list = []
+    for message in messages:
+        msg = service.users().messages().get(userId="me", id=message["id"], format="metadata").execute()
+        headers = msg.get("payload", {}).get("headers", [])
+        for header in headers:
+            if header["name"] == "Subject":
+                subject = header["value"]
+            elif header["name"] == "From":
+                sender = header["value"]
+            elif header["name"] == "Date":
+                date = header["value"]
+            elif header["name"] == "To":
+                recipient = header["value"]
+
+        email_dict = {"id": message["id"],
+                      "thread_id": msg.get("threadId"),
+                      "subject": subject,
+                      "snippet": msg.get("snippet"),
+                      "sender": sender,
+                      "recipient": recipient,
+                      "date": date}
+        email_list.append(email_dict)
+        
+    return email_list
+
+#def get_email_content(message_id):
+ #   msg = service.users().messages().get(userId="me", id=message["id"], format="metadata").execute()
+
+
+
+
